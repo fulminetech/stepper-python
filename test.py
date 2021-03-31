fe6f5fffa7ffea72caa27104921dc6605554a2e2
marutimuthu <marutimuthu@gmail.com>
2021-03-31 11:39:36 +0530

    Test script

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from gpiozero import LED, PWMLED
 import time
 
-direction = LED(19) # Black
+direction = LED(pin=19, active_high=False, initial_value=1)  # Black
 enable = LED(pin=20, active_high=False, initial_value=1) # White
-step = PWMLED(pin=21, active_high=False, initial_value=1, frequency=1000) # Gray 
+step = PWMLED(pin=21, active_high=False, initial_value=1, frequency=2000) # Gray 
 
 # Data format: Dictonary
 payload = {
@@ -12,11 +12,10 @@ payload = {
     'direction': "Forward",
     'pps': 0,
     'timer1': 20,
-    'timer2': 0
+    'timer2': 10
 }
 
-# t = time.localtime()
-# current_time = time.strftime("%H:%M:%S", t)
+current_time = time.strftime("%H:%M:%S", time.localtime())
 
 def runforward():
     print("Forward")
@@ -24,14 +23,30 @@ def runforward():
     enable.on()
     time.sleep(1)
     step.value = 0.5  # 50% of frequency
-    print(time.localtime())
+    print(current_time)
     time.sleep(payload['timer1'])
-    print(time.localtime())
+    print(current_time)
     step.value = 0  # Off
     enable.off()
 
 
-# Start Server
+def runreverse():
+    print("Reverse")
+    direction.on()
+    enable.on()
+    time.sleep(1)
+    step.value = 0.5  # 50% of frequency
+    print(current_time)
+    time.sleep(payload['timer1'])
+    print(current_time)
+    step.value = 0  # Off
+    enable.off()
+    direction.off()
+
+# Main
 if __name__ == '__main__':
     format = "%(asctime)s: %(message)s"
-    runforward()
+    # runforward()
+    # time.sleep(payload['timer2'])
+    runreverse()
+    # time.sleep(payload['timer2'])
