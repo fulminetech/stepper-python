5a49cd5ebc379fedb509dc50015caab9bcc6f7e4
marutimuthu <marutimuthu@gmail.com>
2021-03-31 11:31:06 +0530

    Test script

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import time
 
 direction = LED(19) # Black
 enable = LED(pin=20, active_high=False, initial_value=1) # White
-step = PWMLED(pin=21, active_high=True, initial_value=0, frequency=500) # Gray 
+step = PWMLED(pin=21, active_high=False, initial_value=1, frequency=500) # Gray 
 
 # Data format: Dictonary
 payload = {
@@ -26,6 +26,7 @@ def runforward():
     step.value = 0.5  # 50% of frequency
     print(current_time)
     time.sleep(payload['timer1'])
+    print(current_time)
     step.value = 0  # Off
     enable.off()
 
